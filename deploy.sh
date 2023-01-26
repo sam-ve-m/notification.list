df7f894fb76b8be3446bd6257e7c301e8047ebc7
Samuel Veloso Marçal <samuel.marcal@ligainvest.com.br>
2023-01-26 10:47:06 -0300

    "build: changed fission strategy to poolmg"

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 fission spec init
-fission env create --spec --name notification-list-env --image nexus.sigame.com.br/fission-notification-list:0.1.0-2 --poolsize 0 --version 3 --imagepullsecret "nexus-v3" --spec
-fission fn create --spec --name notification-list-fn --env notification-list-env --code fission.py --targetcpu 80 --executortype newdeploy --maxscale 3 --requestsperpod 10000 --spec
+fission env create --spec --name notification-list-env --image nexus.sigame.com.br/fission-notification-list:0.1.0-2 --poolsize 2 --graceperiod 3 --version 3 --imagepullsecret "nexus-v3" --spec
+fission fn create --spec --name notification-list-fn --env notification-list-env --code fission.py --executortype poolmgr --requestsperpod 10000 --spec
 fission route create --spec --name notification-list-rt --method GET --url /notification/list --function notification-list-fn
